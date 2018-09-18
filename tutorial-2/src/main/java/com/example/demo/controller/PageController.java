2df36845bcd89445dc9de32fee73a472d3b449c3
shafira.fitri <shafira.fitri>
2018-09-18 13:49:59 +0700

    first commit

--- tutorial-2/src/main/java/com/example/demo/controller/PageController.java
+++ tutorial-2/src/main/java/com/example/demo/controller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
-	@RequestMapping ("/viral2jari")
+	@RequestMapping ("/viral")
 	public String index()
 	{
 		return "viral";
