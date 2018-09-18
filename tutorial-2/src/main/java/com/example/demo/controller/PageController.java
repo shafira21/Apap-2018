781331aa82b72aa5e6ecf71270d4b3d9b4375844
shafira.fitri <shafira.fitri>
2018-09-18 14:53:21 +0700

    second commit

--- tutorial-2/src/main/java/com/example/demo/controller/PageController.java
+++ tutorial-2/src/main/java/com/example/demo/controller/PageController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PageController {
-	@RequestMapping ("/viral2")
+	@RequestMapping ("/viral")
 	public String index()
 	{
 		return "viral";
 	}
+	
+	@RequestMapping ("/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name",name);
+		
+		return "challenge";
+	}
+	
+	
 	
 
 }
